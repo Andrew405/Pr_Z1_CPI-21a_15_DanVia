d9146fc02d5fca07758ffd0725dc3205452b5636
Andrey Dederchuk <adederchuk@icloud.com>
2024-10-04 01:39:43 +0300

    Редактировать проверку геом. данных и Добавить "защиту от дурака"

--- pyYazev.py
+++ pyYazev.py
@@ -22,67 +22,71 @@ print("2. Два радиуса (некорректные геометричес
 print("3. Радиус и две стороны (некорректные геометрические параметры)")    # Выбор режима ввода данных
 choice = int(input())
 
-print("Введите желаемые параметры: ")
-for i in range(choice):
-    G.append(int(input()))
-G = tuple(G)
+if choice > 0 and choice < 4:           # Защита от дурака
+    print("Введите желаемые параметры: ")
+    for i in range(choice):
+        G.append(int(input()))
+    G = tuple(G)
 
-for i in range(N):
-    TF = 0
-    xm = np.round(np.random.uniform(-G[0]-1/30*G[0], G[0]+1/30*G[0], N),2)
-    ym = np.round(np.random.uniform(-G[0]-1/30*G[0], G[0]+1/30*G[0], N),2)
-# x,y, r = map(int, input().split()) #ручной ввод
+    if choice == 1:                     # Проверка корректности геометрических данных
+        for i in range(N):
+            TF = 0
+            xm = np.round(np.random.uniform(-G[0]-1/30*G[0], G[0]+1/30*G[0], N),2)
+            ym = np.round(np.random.uniform(-G[0]-1/30*G[0], G[0]+1/30*G[0], N),2)
+        # x,y, r = map(int, input().split()) #ручной ввод
 
-for i in range(N):
-        x = xm[i]
-        y = ym[i]
-        if module1.fp(x, y, G) == 404:
-            save_log("Некорректные геометрические параметры")
-            print("Некорректные геометрические параметры")
-            break
-        elif (module1.fp(x,y,G)) == True:
-            i+=1
-            hits += 1
-            save_log("True ",x,y)
-        else:
-            save_log("False ",x,y)
-# область пристрелки x in range (-r-1/30*r, r+1/30*r) y in range (-r-1/30*r, r+1/30*r)
+        for i in range(N):
+                x = xm[i]
+                y = ym[i]
+                if (module1.fp(x,y,G)) == True:
+                    hits += 1
+                    save_log("True ",x,y)
+                else:
+                    save_log("False ",x,y)
+        # область пристрелки x in range (-r-1/30*r, r+1/30*r) y in range (-r-1/30*r, r+1/30*r)
 
-A = []
-A.insert(0, ["№", "X", "Y", "P"])
-if len(G) == 1:
-    for i in range(N):
-        x = xm[i]
-        y = ym[i]
-        A.append([int(i+1), float(x), float(y), module1.fp(x, y, G)])
-    # print(A)
-    AMatr = np.matrix(A)
-    # print(AMatr)    #Создание Матрицы numpy
+        A = []
+        A.insert(0, ["№", "X", "Y", "P"])
+        
+        for i in range(N):
+            x = xm[i]
+            y = ym[i]
+            A.append([int(i+1), float(x), float(y), module1.fp(x, y, G)])
+        # print(A)
+        AMatr = np.matrix(A)
+        # print(AMatr)    #Создание Матрицы numpy
 
-B = []
-B.insert(0, ["X", "Y", "P"])
-if len(G) == 1:
-    for i in range(N):
-        x = xm[i]
-        y = ym[i]
-        B.append([float(x), float(y), module1.fp(x, y, G)])
-    # print(B)
-    BMatr = pd.DataFrame(B)     # Создание Датафрейма panda
-    # print(BMatr)        
-    BMatr.to_csv('BMatr.csv', header= False, index= False, sep= ';')
-    BMatr.to_excel('BMatr.xlsx', index= False, header= False)    # Вывод Датафрейма в .csv и .xlsx файлы
+        B = []
+        B.insert(0, ["X", "Y", "P"])
+        
+        for i in range(N):
+            x = xm[i]
+            y = ym[i]
+            B.append([float(x), float(y), module1.fp(x, y, G)])
+        # print(B)
+        BMatr = pd.DataFrame(B)     # Создание Датафрейма panda
+        # print(BMatr)        
+        BMatr.to_csv('BMatr.csv', header= False, index= False, sep= ';')
+        BMatr.to_excel('BMatr.xlsx', index= False, header= False)    # Вывод Датафрейма в .csv и .xlsx файлы
 
-    Bcsv = pd.read_csv('BMatr.csv', delimiter= ';')
-    print(Bcsv)
+        Bcsv = pd.read_csv('BMatr.csv', delimiter= ';')
+        print(Bcsv)
 
-    # Bxlsx = pd.read_excel('BMatr.xlsx')
-    # print(Bxlsx)
+        # Bxlsx = pd.read_excel('BMatr.xlsx')
+        # print(Bxlsx)
 
-probability = (hits / N)*100
+        probability = (hits / N)*100
 
-if len(G) == 1:
-    print(f"Количество выстрелов: {N}")
-    print(f"Количество попаданий: {hits}")
-    print(f"Вероятность попадания: {probability:.2f}%")
+        print(f"Количество выстрелов: {N}")
+        print(f"Количество попаданий: {hits}")
+        print(f"Вероятность попадания: {probability:.2f}%")
+    else:
+        print("Некорректные геометрические параметры")
+        save_log("Некорректные геометрические параметры")
+else:
+    print("Ошибка выбора: такого варианта не существует")
+
+
+
 
 log.close()
